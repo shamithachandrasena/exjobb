2f5e4acff423c5b9adb822d0c533a21df0ad0506
Edgar Bogaard <edgar@bogaard.se>
2022-03-22 14:35:54 +0100

    Adds light funktionality

---
+++ light.py
@@ -0,0 +1,18 @@
+import board
+import signal
+import sys
+import time
+import RPi.GPIO as IO
+
+light = 23
+
+def signal_handler(sig, frame): 
+    IO.cleanup()
+    sys.exit(0)
+
+def set_light(state):
+    if state:
+        IO.output(light, IO.HIGH)
+    else:
+        IO.output(light, IO.LOW)
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import steppers
 import time
+import light
 
 steppers.calibrate_X()
 steppers.calibrate_Y()
@@ -7,8 +8,21 @@ steppers.calibrate_Y()
 while(steppers.arm):
     steppers.move_to(100, 100, steppers.stepper.SINGLE)
     time.sleep(1)
+    light.set_light(True)
+    time.sleep(10)
+    light.set_light(False)
+    time.sleep(1)
     steppers.move_to(50, 50, steppers.stepper.SINGLE)
+    time.sleep(1)
+    light.set_light(True)
+    time.sleep(10)
+    light.set_light(False)
     time.sleep(1)
     steppers.move_to(0, 0, steppers.stepper.SINGLE)
     time.sleep(1)
+    light.set_light(True)
+    time.sleep(10)
+    light.set_light(False)
+    time.sleep(1)
+    
 
