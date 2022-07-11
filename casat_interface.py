8f288475c4fc0de9dce82eb94088e913de608a48
Shamitha Chandrasena <shamitha.chandrasena@hd-wireless.se>
2022-07-11 11:28:19 +0530

    [CAS-1056]  - pick by led , read JSON from CASAT servers

--- casat_interface.py
+++ casat_interface.py
@@ -1,12 +1,31 @@
 import json
+import requests
+from requests.structures import CaseInsensitiveDict
+
+url = "http://localhost:8090/api/pick-by-led/current-state"
+
+headers = CaseInsensitiveDict()
+headers["Accept"] = "application/json"
+
 
 data = {}
 
 def update_data():
     global data
-    with open('example.json') as json_file:
-        data = json.load(json_file)
-        json_file.close()
+    try:
+        resp = requests.get(url, headers=headers)
+        if resp.status_code == 200:
+            data = resp.json()
+        else:
+            with open('example.json') as json_file:
+                data = json.load(json_file)
+                json_file.close()
+    except:
+        with open('example.json') as json_file:
+            data = json.load(json_file)
+            json_file.close()
+    print(data)
+
 
 # Returns item
 def get_item():
@@ -30,8 +49,19 @@ def is_picked():
 
 # Write to file
 def write_to_file(key, value):
-    data[key] = value 
-    with open('example.json', 'w') as outfile:
-        json.dump(data, outfile)
-        outfile.close()
+    data[key] = value
+    print (data)
+    try:
+        resp = requests.post(url, headers=headers, json = [{"propertyName":key,"value":str(value)}])
+        if resp.status_code == 200:
+            data = resp.json()
+        else:
+            with open('example.json', 'w') as outfile:
+                json.dump(data, outfile)
+                outfile.close()
+    except:
+        with open('example.json', 'w') as outfile:
+            json.dump(data, outfile)
+            outfile.close()
+    print(data)
 
